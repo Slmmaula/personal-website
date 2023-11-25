e1650aeb959d7a7d75bc48615461471f7e423980
Slmmaula <117788112+Slmmaula@users.noreply.github.com>
2023-11-25 18:12:37 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,23 +34,67 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
+// Email Send Start
+const form = document.querySelector('form');
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
 
-
-
-function emailSend(){
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
 
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "contact.slmmaula@gmail.com",
-        Password : "D905718DA4132375367EB1206B084A413120",
+        Password : "5C0284BDAAB728871605A74DFD6D0FB66118",
         To : 'contact.slmmaula@gmail.com',
         From : "contact.slmmaula@gmail.com",
-        Subject : "New Contact Form Enquiry",
-        Body : "messageBody"
+        Subject : subject.value,
+        Body : bodyMessage
     }).then(
-    message => alert("Message Sent Succesfully!")
-);
+      message => {
+        if (message == "OK") {
+            Swal.fire({
+                title: "Success!",
+                text: "Message Sent Successfully!",
+                icon: "success"
+              });
+        }
+      }
+    );
 }
+
+function checkInput() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        item.addEventListener("keyup", () => {
+            if (item.value !== "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement,classList.add("error");
+            }    
+        });
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInput()
+
+    sendEmail();
+});
+// Email Send END
 
 // Scroll Reveal
 ScrollReveal({
